c3d8e975f29245079e3db472933bdf3c7c56b94b
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-02-08 17:01:54 -0800

    kth smallest/largest element written

--- Kth_smallest_largest_element.py
+++ Kth_smallest_largest_element.py
@@ -10,3 +10,29 @@
 #   yes, better to write your own
 #Can we use data structures: YES
 #Can we mutate OG list? Sure
+#Can we look stuff up? Yes
+
+#simplest, sort using built in function, from smallest to biggest, then count
+def kth_smallest(k, array):
+    #sort
+    print(array)
+    array.sort()
+    #count
+    print(array[k-1])
+    return array[k-1]
+
+#from biggest to smallest using built-in function, then count
+
+def kth_largest(k, array):
+    #sort
+    array.sort(reverse = True)
+    #count
+    print(array[k-1])
+    return array[k-1]
+
+
+#RUN
+k = 1
+array = [3,4,1,2]
+kth_smallest(k, array)
+kth_largest(k, array)
