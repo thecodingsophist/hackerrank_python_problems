2c642eaa69777f99394ce1e18335ca2ca1c55b57
Zhanxi Ni <ni.zhanxi@gmail.com>
2020-02-20 21:44:10 -0800

    graphs and notes

--- wordLadder.py
+++ wordLadder.py
@@ -28,15 +28,45 @@ class Node:
         self.value = value
         self.neighbors = neighbors
 
-def isOneOff(rungOne, rungTwo):
+def isOneOff(wordOne, wordTwo):
+    nodeOne = Node(wordOne, [])
+    nodeTwo = Node(wordTwo, [])
     totalOff = 0
-    for i in rungOne.node:
-        for j in rungTwo.node:
+    for i in nodeOne.value:
+        for j in nodeTwo.value:
             if i != j:
                 totalOff +=1
                 if totalOff > 1:
                     return False
     return True
+
+#NOTES: review graph traversal problems
+
+class Graph:
+    def __init__(self, beginNode, wordList):
+        self.beginNode = beginNode
+        self.wordList = wordList
+        self.listNodes = []
+        for word in wordList:
+            wordNode = Node(word)
+            if self.link(beginNode, wordNode)
+                wordList.remove(wordNode.value)
+            listNodes.append(beginNode)
+        for word in wordList:
+            wordNode = Node(word)
+            i = 0
+            while i<len(Nodes):
+                nextNode(listNodes[i])
+                if self.link(nextNode, wordNode)
+                    wordList.remove(wordNode.value)
+                i++
+
+    def link(self, nodeOne, nodeTwo):
+        if isOneOff(nodeOne.value, nodeTwo.value):
+            nodeOne.neighbors.append(nodeTwo.value)
+            return True
+        return False
+
 
 def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
     #create the graph
