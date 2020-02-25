f5f0a80a33589fdff0f93cf6f7fefe25b5a8c6dd
Zhanxi Ni <ni.zhanxi@gmail.com>
2020-02-24 23:42:55 -0800

    added init function for adjacency list

--- graph_adjacency.py
+++ graph_adjacency.py
@@ -4,3 +4,14 @@ class AdjNode:
     def __init__(self, data):
         self.vertex = data
         self.next = None
+
+# A class to represent a graph. A graph is the list of the adjacency lists.
+# Size of the array will be the no. of the vertices "V"
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [None] * self.V # Not sure if I understand this
+
+    # Function to add an edge in an undirected graph
+    def
